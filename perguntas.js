8486ec5633df721089a4b8c2263cde7b714b6177
laizmbt18 <144949196+laizmbt18@users.noreply.github.com>
2024-10-17 22:10:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quando ocorreu a 2 guerra?',
+    'Entre 1939 ate 1945'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha e Madri'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é dengue?',
+    'Um mosquito transmitido pelo Aedes aegypti',
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tudo bem em Inglês?',
+    'all good'
+)
